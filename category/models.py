eaf06d74d528dbeb1b0c4ec5efd7fa0c8b081361
Mahmoud Ghazy <64809988+Mahmoudghazy680@users.noreply.github.com>
2020-07-24 23:33:22 +0200

    1
    1

---
+++ category/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
+
+class Service(models.Model):
+    servname     = models.CharField(default='Service_name', max_length=50, blank=False , null=False,verbose_name=_('Service Name')) 
+    mainserv     = models.ForeignKey('self' , limit_choices_to={'mainserv__isnull' : True} , on_delete=models.CASCADE , blank=True , null=True , verbose_name=_('Main Service'))
+    servimg      = models.ImageField(upload_to='servimg/', blank=True , null=True , verbose_name=_('Service Image'))
+    servdesc     = models.TextField(blank=True , null=True, verbose_name=_('Service Decsription'))
+
+
+
+    def __str__(self):
+        return self.servname
---
+++ client/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+
+class client(models.Model):
+    pass 
